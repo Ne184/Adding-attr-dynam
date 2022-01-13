e3b35c859978dafc0935da1edebc1dd86bc8e9b3
Ne184 <nevena.gincheva@abv.bg>
2022-01-13 22:26:15 +0200

    Added data-price, not working

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+ const productElements = document.querySelectorAll(".product");
+ productElements.forEach(product => {
+    const priceAttribute = product.querySelector(".price");  
+    const priceText = priceAttribute.innerText;
 
+    product.setAttribute("data-price", priceText);
+ })
 
 });
